ecfab62885e217053d4c2e1da65f597328d6b394
your-github-radieuss <your-github-DARIEUSANG.23@ichat.sp.edu.sg>
2024-02-21 03:14:04 +0800

    Changes 1.ping member when leave vc 2.personal welcome message 3.

--- src/index.js
+++ src/index.js
@@ -7,25 +7,58 @@ const client=new Client({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildVoiceStates,
     ]
 })
 
 //c.user.tag gives username#____
 //c.user.username gives username
 //c.user.id gives id
-client.on('ready', (c)=>{//c stands for client, but cant redefine it
-    console.log(`DARWIZZYS🐐 ALIVE`)
+client.on('ready', ()=>{//c stands for client, but cant redefine it
+    console.log('DARWIZZYS🐐 ALIVE')
+    // client.guilds.cache.forEach(guild => {
+    //     const generalChannel = guild.channels.cache.find(channel => channel.name == 'general' && channel.type == 0);
+    //     if (generalChannel) {
+    //         generalChannel.send('DARWIZZYS🐐 ALIVE')
+    //     } else {
+    //         console.error(`'general' channel not found in ${guild.name}.`);
+    //     }
+    // });
 })
 client.on('messageCreate', (message)=>{  
-    if(message.author.bot){//if bot send msg, do nth
+    if(message.author.bot||message.system){//if bot send msg, do nth
         return;
     }
-
+    if(message.author.username=='antheyuhh'){//if bot send msg, do nth
+        message.reply(`Fuck Off ${message.author}`)
+        return;
+    }
     if(message.content=='hello'){  
         message.reply('Fuck Off')
     }else{
-        message.reply('DARWIZZYS THE GOAT')
+        message.reply('DARWIZZYS THE GOAT🐐🐐')
     }
     
 })
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.find(c => c.name === "general").send(`WELCOME OUR NEWEST MEMBER ${member.user} TO THE CCP`);
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    const guild = newState.guild
+    const generalChannel = guild.channels.cache.find(channel => channel.name == 'ping-call' && channel.type == 0);
+    if (!generalChannel) {
+        console.error(`General text channel not found in ${guild.name}.`);
+        return;
+    }
+    if (oldState.channel !== newState.channel) {
+        // User joined or left a voice channel
+        if (oldState.channel) {
+            generalChannel.send(`WHY YOU LEAVE ${oldState.member}`);
+        }
+    }
+});
+
+
 client.login(process.env.Token)
